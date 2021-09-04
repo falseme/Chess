876caf82fd69974c239639cb43033a4669ada6e0
Fabricio-Tomas <78918951+Fabricio-Tomas@users.noreply.github.com>
2021-09-04 06:00:25 -0300

    Added promotion rule
    Now pawns can be promoted when in last row

--- src/event/SquareMouse.java
+++ src/event/SquareMouse.java
@@ -3,6 +3,8 @@ package event;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JOptionPane;
+
 import main.App;
 import main.Piece;
 import ui.Square;
@@ -48,6 +50,7 @@ public class SquareMouse extends MouseAdapter {
 
 			moving.getPiece().moved = true;
 
+			// if it is a king > check if the movement is a castling
 			if (moving.getPiece() == Piece.wKing && owner.getPiece() == Piece.wRock
 					|| moving.getPiece() == Piece.bKing && owner.getPiece() == Piece.bRock) {
 
@@ -66,10 +69,59 @@ public class SquareMouse extends MouseAdapter {
 				moving.addPiece(null);
 				owner.addPiece(null);
 
-			} else {
+			} else { // normal movement
 
 				owner.addPiece(moving.getPiece());
 				moving.addPiece(null);
+
+			}
+
+			// check if pawn is at last row
+			if (owner.getPos()[1] == 0 && owner.getPiece() == Piece.wPawn
+					|| owner.getPos()[1] == 7 && owner.getPiece() == Piece.bPawn) {
+
+				String[] options = new String[] { "Dama", "Torre", "Caballo", "Alfil" };
+
+				int selection = JOptionPane.showOptionDialog(null, "message", "title", JOptionPane.DEFAULT_OPTION,
+						JOptionPane.PLAIN_MESSAGE, null, options, null);
+
+				switch (selection) {
+
+				case 0:
+
+					if (owner.getPiece().isWhiteTeam())
+						owner.addPiece(Piece.wQueen);
+					else
+						owner.addPiece(Piece.bQueen);
+
+					break;
+				case 1:
+
+					if (owner.getPiece().isWhiteTeam())
+						owner.addPiece(Piece.wRock);
+					else
+						owner.addPiece(Piece.bRock);
+
+					break;
+
+				case 2:
+
+					if (owner.getPiece().isWhiteTeam())
+						owner.addPiece(Piece.wHorse);
+					else
+						owner.addPiece(Piece.bHorse);
+
+					break;
+				case 3:
+
+					if (owner.getPiece().isWhiteTeam())
+						owner.addPiece(Piece.wBishop);
+					else
+						owner.addPiece(Piece.bBishop);
+
+					break;
+
+				}
 
 			}
 
